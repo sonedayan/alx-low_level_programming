51ec098243df379426dff035706de184be47b03f
Dayan Sone <sonedayan@gmail.com>
2023-03-22 17:37:42 -0700

    prints the aiphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - A function that prints the alphabet
+ * Return: returns void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
