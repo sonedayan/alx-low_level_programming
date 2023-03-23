53e43e5fda6c6ab86909e10a4425806b34ae6748
Dayan Sone <sonedayan@gmail.com>
2023-03-22 20:00:24 -0700

    print till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints till 98
+ *
+ * @n: parameter
+ *
+ * Return: returns n
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if(n >= 0 && n <= 98)
+	{
+		for(i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar('\n');
+		}
+		else if (n < 0)
+		{
+			for (i = n; i <= 98; i++)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar('\n');
+			}
+		}
+}
