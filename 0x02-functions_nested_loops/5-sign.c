11e9700afeac40e1f5a49da60915c451fdc2541e
Dayan Sone <sonedayan@gmail.com>
2023-03-22 18:48:13 -0700

    print out sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign
+ *
+ * @n: parameter needed
+ *
+ * Return: 1 if positive, -1 if negative, 0 if neutral
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
