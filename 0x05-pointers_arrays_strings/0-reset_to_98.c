44c059f2f2dd0874f1ca35899a1f3b40df62daf3
Dayan Sone <sonedayan@gmail.com>
2023-03-27 17:55:18 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - function taking pointer as parameter
+ *
+ * @n: parameter to be passed
+ *
+ * Return: returns n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
